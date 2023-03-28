a3c9f322d3b557cd0c9b0bcfde60c181b2599c3f
patelamancode <74897715+patelamancode@users.noreply.github.com>
2023-03-28 18:18:50 +0530

    first day code done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import Carstore from './components/Carstore';
-import Cart from './components/Cart';
-import ClassStudents from './components/ClassStudents';
-import Cookbook from './components/Cookbook';
-import Cookbookvc from './components/Cookbookvc';
-import Library from './components/Library';
-import Onlinestore from './components/Onlinestore';
-import Watchlist from './components/Watchlist';
-import Geolocation from './components/Geolocation';
-import Levelzeroreact from './components/Levelzeroreact';
-;
+import { employees } from './components/Database';
+// import Carstore from './components/Carstore';
+// import Cart from './components/Cart';
+// import ClassStudents from './components/ClassStudents';
+// import Cookbook from './components/Cookbook';
+// import Cookbookvc from './components/Cookbookvc';
+// import Library from './components/Library';
+// import Onlinestore from './components/Onlinestore';
+// import Watchlist from './components/Watchlist';
+// import Geolocation from './components/Geolocation';
+// import Levelzeroreact from './components/Levelzeroreact';
+import EmployeeCard from './components/100Days_of_code/EmployeeCard';
 
 
 
@@ -27,8 +28,8 @@ function App() {
       <Cookbook /> */}
       {/* <Cookbookvc name = "Aman as a prop"/> */}
       {/* <Geolocation /> */}
-      < Levelzeroreact />
-     
+      {/* < Levelzeroreact /> */}
+     <EmployeeCard employeesDetails={employees}/>
     </div>
   )
 }
---
+++ src/components/100Days_of_code/EmployeeCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const EmployeeCard = ({employeesDetails}) => {
+    const whoIsLevel2 = (level) => {if(level===2) return "red"};
+    const borderForPresident = (designation) => {if(designation==="president") return "solid 2px black"};
+    
+  return (
+    <div>
+      <h1>Employee Details</h1>
+      {employeesDetails.map(({ name, level, dept, designation, salary }) =>
+        <li style={{color: whoIsLevel2(level), border: borderForPresident(designation)}}>name: {name} , Level: {level}, Department: {dept}, Designation: {designation} & Salary: {salary}</li> )}
+        <h3>Total company expense for salary: {employeesDetails.reduce((acc, {salary}) => acc+salary ,0)}</h3>
+    </div>
+  )
+}
+
+export default EmployeeCard;
